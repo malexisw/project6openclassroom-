113dedee36b7c64f8c1d0a9370d79ccd05105717
Alexis Martel <alexis.martel.w@hotmail.com>
2023-02-22 13:16:56 +0100

    🔧 Add config files

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+  dbUrl: "",
+  port: 3000,
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const { dbUrl } = require("./config");
+const { port } = require("./config");
+const userRouter = require("./routes/Users");
+const saucesRouter = require("./routes/Sauces");
+
+const mongoDB = dbUrl;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(userRouter);
+app.use(saucesRouter);
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.listen(port, () => {
+  console.log("Server is running at port " + port);
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
